d4274274722c018c03f173d2c88035e86a9b4607
JinalGangani <jinalgangani1@gmail.com>
2021-10-16 13:58:23 +0530

    Initial commit

---
+++ question.js
@@ -0,0 +1,116 @@
+const question = [
+    {
+        qno: 1,
+        que: "To Create Mutable Object __ is Used",
+        a: "Var",
+        b: "Let",
+        c: "Both Var & Let",
+        d: "None of the above.",
+        ans : "a",
+        desc : "a is the correct option, var is used to create a mutable object"
+
+    },
+    {
+        qno: 2,
+        que: "IOS Is A __software",
+        a: "Proprietary",
+        b: "Free and Open Source",
+        c: "Both a & b",
+        d: "None of the above.",
+        ans : "a",
+        desc : "a is the correct option, IOS is a Proprietary software."
+
+
+    },
+    {
+        qno: 3,
+        que: "Which Of The Following Is Incorrect Value Type Of The Swift?",
+        a: "Enum",
+        b: "Class",
+        c: "Double",
+        d: "Character",
+        ans : "b",
+        desc :"b is Correct option,class is incorrect value type of the swift "
+
+    },
+    {
+        qno: 4,
+        que: "What Is Bundle In IOS",
+        a: "It is a class",
+        b: "It is used to send data",
+        c: "It is floder with .app extension",
+        d: "None of the above.",
+        ans: "c",
+        desc: "c is Correct option, it is floder with .app extension"
+
+    },
+    {
+        qno: 5,
+        que: "The IDE Used In Swift Is ..........",
+        a: "Ld",
+        b: "Gas",
+        c: "Xcode",
+        d: "Swiftc",
+        ans: "c",
+        desc: "c is Correct option, The IDE used in swift is Xcode."
+
+    },
+    {
+        qno: 6,
+        que: "Swift is which kind of language?",
+        a: "Type safe language",
+        b: "scripting language",
+        c: "Object oriented programming language",
+        d: "None of the above.",
+        ans: "d",
+        desc: "d is Correct option."
+
+    },
+    
+    {
+        qno: 7,
+        que: "We use the keyword ............ to create contants in swift.",
+        a: "Let",
+        b: "Const",
+        c: "Contants",
+        d: "None of the above.",
+        ans: "a",
+        desc: "a is Correct option, we use the keyword Let to create contants in swift."
+
+    },
+    {
+        qno: 8,
+        que: "Which Of The Following Declares A Mutable Array In Swift?",
+        a: "Let x = [Int]",
+        b: "Var x = [Int]",
+        c: "Let x = [Int]()",
+        d: "var x = [Int]()",
+        ans: "d",
+        desc: "c is Correct option,"
+
+    },
+    {
+        qno: 9,
+        que: "............ keyword in swift used to initialize the variable?",
+        a: "nil",
+        b: "nul",
+        c: "NIL",
+        d: "NULL",
+        ans: "a",
+        desc: "a is Correct option, nil keyword in swift used to initialize the variable"
+
+    },
+    {
+        qno: 10,
+        que: "What is the user interface of Iphone OS called?",
+        a: "Cocoa",
+        b: "Mobiface",
+        c: "Touchwiz",
+        d: "None of the above",
+        ans: "c",
+        desc: "c is Correct option,"
+
+    }
+
+]
+module.exports = question;
